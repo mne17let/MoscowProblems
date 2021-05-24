846d5d4e1b9ce5e029758efef6db4675983de049
“Vladimir <vladimirsemovskih@gmail.com>
2021-05-24 22:44:58 +0400

    Добавил layout для фрагмента с описанием проблемы

---
+++ app/src/main/java/com/example/moscowproblems/Models/ProblemModel.kt
@@ -0,0 +1,8 @@
+package com.example.moscowproblems.Models
+
+import java.util.*
+
+data class ProblemModel(val id: UUID = UUID.randomUUID(),
+                        var title: String,
+                        var date: Date = Date(),
+                        var isSolved: Boolean = false)
